a998d9a2ddc165636b01661fd2715afa3d991950
Volodymyr Kulyk <kulyk.vr@gmail.com>
2024-03-20 19:53:41 +0200

    ready to deploy

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,16 @@ burgerBtn.onclick = () => {
   headerLogo.classList.toggle("header__logo_invisible");
   headerNavbar.classList.toggle("header__navbar_visible");
 };
+
+const productsBtn = document.querySelector(".products__info-button");
+const productsContent = document.getElementById("products-content");
+const productsBtnText = document.querySelector(".products__info-button span");
+const productsBtnAngle = document.querySelector(".products__info-button svg");
+
+productsBtn.onclick = () => {
+  productsContent.classList.toggle("products__inner_full-height");
+  productsBtnText.innerText === "Show More"
+    ? (productsBtnText.innerText = "Show Less")
+    : (productsBtnText.innerText = "Show More");
+  productsBtnAngle.classList.toggle("button-arrow-rotate");
+};
